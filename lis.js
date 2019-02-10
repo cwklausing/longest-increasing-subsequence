eb56e462255b008f091242227ac556372bf90cc6
cwklausing <cwklausing@gmail.com>
2019-02-10 14:55:49 -0600

    update algorithm to use actual LIS array

--- lis.js
+++ lis.js
@@ -4,29 +4,30 @@ export const longestIncSubSeqString = (str) => {
   if(str.length === 1) {
     return 0
   }
-  const arr = str.split('').map((letter) => ({
-    value: letter.charCodeAt(0) - 96,
-    lis: 1
-  }))
+  const arr = str.split('').map((letter) => letter.charCodeAt(0) - 96)
 
-  return longestIncSubSeq(arr)
+  return str.length - longestIncSubSeq(arr).length
 }
 
-const longestIncSubSeq = (arr) => {
+// arr: array of numbers
+export const longestIncSubSeq = (initArray) => {
+  const arr = initArray.map(number => ({
+    value: number,
+    lis: [number],
+  }))
+
   for(let i = 1; i < arr.length; i++) {
     for(let j = 0; j < i; j++) {
       if(arr[j].value <= arr[i].value) {
-        let newLis = arr[j].lis + 1;
-        if(newLis > arr[i].lis) {
+        let newLis = [...arr[j].lis, arr[i].value]
+        if(newLis.length > arr[i].lis.length) {
           arr[i].lis = newLis
         }
       }
     }
   }
 
-  const lisNum = arr.reduce((acc, item) => {
-    return item.lis > acc ? acc = item.lis : acc
-  }, 1)
-  
-  return arr.length - lisNum
+  return arr.reduce((acc, item) => {
+    return item.lis.length > acc.length ? acc = item.lis : acc
+  }, [])
 }
--- lis.test.js
+++ lis.test.js
@@ -1,6 +1,6 @@
-import { longestIncSubSeqString } from './lis'
+import { longestIncSubSeqString, longestIncSubSeq } from './lis'
 
-const testData = [
+const testStrings = [
   {
     input: 'floccinaucinihilipilification',
     expected: 17,
@@ -19,11 +19,42 @@ const testData = [
   },
 ]
 
+const testArrays = [
+  {
+    input: [10,4,7,18,38,2,18],
+    expected: [4,7,18,38],
+  },
+  {
+    input: [10,4,7,18,38,2,3,18,4,6,6,7,19,30],
+    expected: [2,3,4,6,6,7,19,30],
+  },
+  {
+    input: [10,12,16,4,10,18,3,12,3,8,45,65,6,17,9,30],
+    expected: [10,12,16,18,45,65],
+  },
+  {
+    input: [10,9,8,7,6,5,4,3,2,1],
+    expected: [10]
+  },
+  {
+    input: [10,9,8,7,6,5,4,3,2,1,9],
+    expected: [9,9]
+  },
+]
+
 
 describe('longestIncSubSeqString', () => {
-  it('should return expected output', () => {
-    testData.forEach((value) => {
+  it('should return fewest number of letters needed to remove to create LIS', () => {
+    testStrings.forEach((value) => {
       expect(longestIncSubSeqString(value.input)).toBe(value.expected)
+    })
+  })
+})
+
+describe('longestIncSubSeq', () => {
+  it('should return array of longest increasing subsequence', () => {
+    testArrays.forEach((value) => {
+      expect(longestIncSubSeq(value.input)).toEqual(value.expected)
     })
   })
 })
